520bd485a85e75c8b7b6935a15a66a84b2fc57bf
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-27 14:23:54 +0300

    Add animation with framer motion

--- src/components/ToDoShow.jsx
+++ src/components/ToDoShow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { motion } from "framer-motion";
 import { AiFillEdit } from "react-icons/ai";
 import { AiFillDelete } from "react-icons/ai";
 
@@ -14,23 +14,30 @@ export default function ToDoShow({ singleToDo, deleteTask, editTask }) {
   return (
     <div className="mt-6 w-full md:w-[550px]">
       <ul>
-        <li className="bg-gray-400 text-white py-5 rounded-lg text-2xl px-5 flex justify-between">
+        <motion.li
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          className="bg-gray-400 text-white py-5 rounded-lg text-2xl px-5 flex justify-between"
+        >
           {singleToDo.todoName}
           <div className="flex items-center">
-            <span
+            <motion.span
+              whileHover={{ scale: 1.2 }}
               className="text-yellow-600 text-3xl cursor-pointer mr-4 hover:text-yellow-800 transition-colors duration-300"
               onClick={handleEdit}
             >
               <AiFillEdit />
-            </span>
-            <span
+            </motion.span>
+            <motion.span
+              whileHover={{ scale: 1.2 }}
               className="text-red-400 text-3xl cursor-pointer hover:text-red-600 transition-colors duration-300"
               onClick={handleDelete}
             >
               <AiFillDelete />
-            </span>
+            </motion.span>
           </div>
-        </li>
+        </motion.li>
       </ul>
     </div>
   );
