a93ceb973ae17ce7551652c39d4a778a314ffb2f
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-27 10:59:19 +0300

    Add and configure tailwindcss for styling

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageTitle = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-400">Hello world!</h1>
+    </div>
+  );
+};
+
+export default PageTitle;
