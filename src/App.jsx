584406210710b7c8190744aba009784d4d7d9c99
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-27 14:01:59 +0300

    Render all components in the parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-import PageTitle from './components/PageTitle'
+import Header from "./components/Header"
+import TodoForm from "./components/TodoForm"
 
 function App() {
 
 
   return (
-    <div className="">
-      <PageTitle/>
+    <div className="w-full bg-gray-200  h-screen p-4">
+      <div className="bg-white max-w-4xl mx-auto flex flex-col items-center h-full text-center">
+      <Header/>
+      <TodoForm/>
+      </div>
+      
+      
     </div>
   )
 }
