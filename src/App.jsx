42dd488c3169e5735aade952af14395837e34cdd
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-28 09:37:38 +0300

    Adjust screen height and add padding bottom

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
-    <div className="w-full bg-gray-200  h-screen p-4">
-      <div className="bg-white max-w-4xl mx-auto flex flex-col items-center h-full text-center">
+    <div className="w-full bg-gray-200  h-full p-4">
+      <div className="bg-white max-w-4xl mx-auto flex flex-col items-center text-center pb-4">
         <Header />
         <TodoForm />
       </div>
