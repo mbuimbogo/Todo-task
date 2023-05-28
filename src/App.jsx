d07bb2117c4803310d0e672fb12c5feda009c74a
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-28 09:53:51 +0300

    adjust screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoForm from "./components/TodoForm";
 function App() {
   return (
     <div className="w-full bg-gray-200  h-full p-4">
-      <div className="bg-white max-w-4xl mx-auto flex flex-col items-center text-center pb-4">
+      <div className="bg-white max-w-4xl mx-auto flex flex-col items-center max-h-full text-center pb-4">
         <Header />
         <TodoForm />
       </div>
